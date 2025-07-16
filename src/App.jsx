59170df02f14ef7713c4206f6225831fe652547f
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-07-16 17:22:22 -0500

    with react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  // computational logic and state management
+
+
+  return (
+    // everything presentational in the browser
+    <>
+      <h1>Hi Mom</h1>
+    </>
+  )
+}
+
+export default App
